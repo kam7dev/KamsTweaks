2474614a90ac5cd82fb99c213988efff312e3fb3
km7dev <gavin@t8er.com>
2025-07-15 01:59:45 -0400

    Entity protections

--- src/main/java/kam/kamsTweaks/features/landclaims/LandClaimsProtection.java
+++ src/main/java/kam/kamsTweaks/features/landclaims/LandClaimsProtection.java
@@ -4,8 +4,8 @@ import kam.kamsTweaks.ItemManager;
 import kam.kamsTweaks.KamsTweaks;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.TNTPrimed;
+import org.bukkit.entity.*;
+import org.bukkit.entity.minecart.StorageMinecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -13,7 +13,13 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.hanging.HangingBreakByEntityEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
+import org.bukkit.inventory.InventoryHolder;
 
 public class LandClaimsProtection implements Listener {
     LandClaims lc;
@@ -86,6 +92,99 @@ public class LandClaimsProtection implements Listener {
     }
 
     @EventHandler
+    public void onInvOpen(InventoryOpenEvent e) {
+        InventoryHolder entity = e.getInventory().getHolder();
+        if (entity instanceof AbstractHorse || entity instanceof ChestBoat || entity instanceof StorageMinecart) {
+            Player player = (Player) e.getPlayer();
+            LandClaims.Claim claim = lc.getClaim(((Entity) entity).getLocation());
+            if (!lc.hasPermission(player, claim, LandClaims.ClaimPermission.INTERACT)) {
+                if (player.hasPermission("kamstweaks.landclaims.bypass")) {
+                    player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(", but you are bypassing the claim.")));
+                    return;
+                }
+                player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(".")));
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityInteract(PlayerInteractEntityEvent e) {
+        Entity entity = e.getRightClicked();
+        if (entity instanceof ItemFrame || entity instanceof ArmorStand) {
+            Player player = e.getPlayer();
+            LandClaims.Claim claim = lc.getClaim(entity.getLocation());
+            if (!lc.hasPermission(player, claim, LandClaims.ClaimPermission.INTERACT)) {
+                if (player.hasPermission("kamstweaks.landclaims.bypass")) {
+                    player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(", but you are bypassing the claim.")));
+                    return;
+                }
+                player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(".")));
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onHangDestroy(HangingBreakByEntityEvent e) {
+        if (e.getRemover() instanceof Player player) {
+            LandClaims.Claim claim = lc.getClaim(e.getEntity().getLocation());
+            if (!lc.hasPermission(player, claim, LandClaims.ClaimPermission.BLOCKS)) {
+                if (player.hasPermission("kamstweaks.landclaims.bypass")) {
+                    player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(", but you are bypassing the claim.")));
+                    return;
+                }
+                player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(".")));
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onVehicleDestroy(VehicleDestroyEvent e) {
+        Entity entity = e.getVehicle();
+        if (!(entity instanceof AbstractHorse || entity instanceof ChestBoat || entity instanceof StorageMinecart)) return;
+        if (e.getAttacker() instanceof Player player) {
+            LandClaims.Claim claim = lc.getClaim(e.getVehicle().getLocation());
+            if (!lc.hasPermission(player, claim, LandClaims.ClaimPermission.BLOCKS)) {
+                if (player.hasPermission("kamstweaks.landclaims.bypass")) {
+                    player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(", but you are bypassing the claim.")));
+                    return;
+                }
+                player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(".")));
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityHit(EntityDamageByEntityEvent e) {
+        Entity entity = e.getEntity();
+        if (!(entity instanceof ItemFrame || entity instanceof ArmorStand || entity instanceof AbstractHorse || entity instanceof ChestBoat || entity instanceof StorageMinecart)) return;
+        if (e.getDamager() instanceof Player player) {
+            LandClaims.Claim claim = lc.getClaim(e.getEntity().getLocation());
+            if (!lc.hasPermission(player, claim, LandClaims.ClaimPermission.BLOCKS)) {
+                if (player.hasPermission("kamstweaks.landclaims.bypass")) {
+                    player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(", but you are bypassing the claim.")));
+                    return;
+                }
+                player.sendMessage(Component.text("This land is claimed by ").append(Component.text(claim.m_owner == null ? "the server" : claim.m_owner.getName() == null ? "Unknown player" : claim.m_owner.getName()).color(NamedTextColor.GOLD)).append(Component.text(".")));
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityInteractAt(PlayerInteractAtEntityEvent e) {
+        onEntityInteract(e);
+    }
+//
+//    @EventHandler
+//    public void onEntityInteractAS(PlayerArmorStandManipulateEvent e) {
+//        onEntityInteract(e);
+//    }
+
+    @EventHandler
     public void onTnt(EntityExplodeEvent e) {
         if (!KamsTweaks.getInstance().getConfig().getBoolean("land-claims.enabled", true)) return;
         if (e.getEntity() instanceof TNTPrimed tnt) {
@@ -107,6 +206,12 @@ public class LandClaimsProtection implements Listener {
                     tnt.remove();
                 }
             }
+        } else if (e.getEntity() instanceof EnderCrystal ec) {
+            LandClaims.Claim claim = lc.getClaim(e.getEntity().getLocation());
+            if (!lc.hasPermission(null, claim, LandClaims.ClaimPermission.BLOCKS)) {
+                e.setCancelled(true);
+                ec.remove();
+            }
         }
     }
 
@@ -124,6 +229,11 @@ public class LandClaimsProtection implements Listener {
                 if (!lc.hasPermission(null, claim, LandClaims.ClaimPermission.BLOCKS)) {
                     event.setCancelled(true);
                 }
+            }
+        } else if (event.getDamager() instanceof EnderCrystal ec) {
+            LandClaims.Claim claim = lc.getClaim(ec.getLocation());
+            if (!lc.hasPermission(null, claim, LandClaims.ClaimPermission.BLOCKS)) {
+                event.setCancelled(true);
             }
         }
     }
