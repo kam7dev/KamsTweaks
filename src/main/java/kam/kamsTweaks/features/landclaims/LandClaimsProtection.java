05df62c4070f696293315494801b81acc7b199c0
km7dev <gavin@t8er.com>
2025-07-31 01:03:49 -0400

    Allow interacting with noninteractable blocks

--- src/main/java/kam/kamsTweaks/features/landclaims/LandClaimsProtection.java
+++ src/main/java/kam/kamsTweaks/features/landclaims/LandClaimsProtection.java
@@ -2,6 +2,7 @@ package kam.kamsTweaks.features.landclaims;
 
 import kam.kamsTweaks.ItemManager;
 import kam.kamsTweaks.KamsTweaks;
+import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import net.kyori.adventure.text.Component;
@@ -101,6 +102,72 @@ public class LandClaimsProtection implements Listener {
         }
     }
 
+    // Slightly altered from https://www.spigotmc.org/threads/check-if-a-block-is-interactable.535861/
+    // Originally by LoneDev
+
+    public static boolean isInteractable(Block block){
+        Material type = block.getType();
+        @SuppressWarnings("deprecation")
+        boolean interactable = type.isInteractable();
+        if (!interactable) return false;
+        switch (type) {
+            case ACACIA_STAIRS:
+            case ANDESITE_STAIRS:
+            case BAMBOO_STAIRS:
+            case BAMBOO_MOSAIC_STAIRS:
+            case BIRCH_STAIRS:
+            case BLACKSTONE_STAIRS:
+            case BRICK_STAIRS:
+            case COBBLESTONE_STAIRS:
+            case CRIMSON_STAIRS:
+            case DARK_OAK_STAIRS:
+            case DARK_PRISMARINE_STAIRS:
+            case DIORITE_STAIRS:
+            case END_STONE_BRICK_STAIRS:
+            case GRANITE_STAIRS:
+            case JUNGLE_STAIRS:
+            case MOSSY_COBBLESTONE_STAIRS:
+            case MOSSY_STONE_BRICK_STAIRS:
+            case NETHER_BRICK_STAIRS:
+            case OAK_STAIRS:
+            case PALE_OAK_STAIRS:
+            case POLISHED_ANDESITE_STAIRS:
+            case POLISHED_BLACKSTONE_BRICK_STAIRS:
+            case POLISHED_BLACKSTONE_STAIRS:
+            case POLISHED_DIORITE_STAIRS:
+            case POLISHED_GRANITE_STAIRS:
+            case PRISMARINE_BRICK_STAIRS:
+            case PRISMARINE_STAIRS:
+            case PURPUR_STAIRS:
+            case QUARTZ_STAIRS:
+            case RED_NETHER_BRICK_STAIRS:
+            case RED_SANDSTONE_STAIRS:
+            case SANDSTONE_STAIRS:
+            case SMOOTH_QUARTZ_STAIRS:
+            case SMOOTH_RED_SANDSTONE_STAIRS:
+            case SMOOTH_SANDSTONE_STAIRS:
+            case SPRUCE_STAIRS:
+            case STONE_BRICK_STAIRS:
+            case STONE_STAIRS:
+            case WARPED_STAIRS:
+            case ACACIA_FENCE:
+            case BAMBOO_FENCE:
+            case BIRCH_FENCE:
+            case CRIMSON_FENCE:
+            case DARK_OAK_FENCE:
+            case JUNGLE_FENCE:
+            case MOVING_PISTON:
+            case NETHER_BRICK_FENCE:
+            case OAK_FENCE:
+            case PALE_OAK_FENCE:
+            case SPRUCE_FENCE:
+            case WARPED_FENCE:
+                return false;
+            default: return true;
+        }
+
+    }
+
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
         if (e.getPlayer().getTargetEntity(5) instanceof Creature)
@@ -136,7 +203,7 @@ public class LandClaimsProtection implements Listener {
         }
         if (!KamsTweaks.getInstance().getConfig().getBoolean("land-claims.enabled", true))
             return;
-        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK && isInteractable(e.getClickedBlock())) {
             Player player = e.getPlayer();
             assert e.getClickedBlock() != null;
             LandClaims.Claim claim = lc.getClaim(e.getClickedBlock().getLocation());
