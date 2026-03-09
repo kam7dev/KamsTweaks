e309959bb13b8e4a5b41e3d07d969eb097632cb2
km7dev <100500023+Kingminer7@users.noreply.github.com>
2026-03-08 23:16:31 -0400

    Update ClaimProtections.java

--- src/main/java/kam/kamsTweaks/features/claims/ClaimProtections.java
+++ src/main/java/kam/kamsTweaks/features/claims/ClaimProtections.java
@@ -1190,6 +1190,7 @@ public class ClaimProtections implements Listener {
     @EventHandler(priority = EventPriority.HIGH)
     public void EConEntityInteract(PlayerInteractEntityEvent e) {
         if (!KamsTweaks.getInstance().getConfig().getBoolean("entity-claims.enabled", true)) return;
+		if (isClaimable(e.getRightClicked()) {
             if (ItemManager.getType(e.getPlayer().getInventory().getItemInMainHand()) == ItemManager.ItemType.CLAIM_TOOL) {
                 e.setCancelled(true);
                 if (e.getRightClicked() instanceof Tameable tameable && tameable.getOwnerUniqueId() != null) {
@@ -1233,6 +1234,7 @@ public class ClaimProtections implements Listener {
                 message(e.getPlayer(), Component.text("You don't have permission to interact with this entity! (Entity claimed by ").append(Names.instance.getRenderedName(claim.owner), Component.text(")")));
                 e.setCancelled(true);
             }
+		}
     }
 
     @EventHandler
